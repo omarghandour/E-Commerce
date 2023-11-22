6f07ff10c59bb9e92e0cdce92b1b65ed32c229be
omarghandour <alex.forget50@gmail.com>
2023-11-23 01:14:14 +0200

    ul557

--- app/offers/page.jsx
+++ app/offers/page.jsx
@@ -1,11 +1,10 @@
-import PushNotificationLayout from "../components/PushNotificationLayout";
-import styles from "../styles/Home.module.css";
+import PushNotificationLayout from "@/components/PushNotificationLayout";
 
 export default function Offers() {
   return (
     <PushNotificationLayout>
-      <div className={styles.container}>
-        <main className={styles.main}>
+      <div>
+        <main>
           <h2>Offers Page</h2>
           <p>50% discount on T shirts</p>
         </main>
