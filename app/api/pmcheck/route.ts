2beb2b0a8b15e3ad165c9c7a264e47ab84138012
omarghandour <alex.forget50@gmail.com>
2023-12-16 14:58:47 +0200

    fixing paying paymob

--- app/api/pmcheck/route.ts
+++ app/api/pmcheck/route.ts
@@ -1,3 +1,7 @@
 export async function POST(request: Request) {
+const data = await request.json();
+// const response = await this.client.post (request => request && request.successful === true ? JSON.deserialize( JSON.stringify( data ) ) : data ) ; // TODO
+console.log(data);
 
+return  new Response(JSON.stringify({data: data}))
 }
